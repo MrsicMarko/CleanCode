a82fd74442d526193d31cf51ad7d6b2975ec57b9
MrsicMarko <marko2605@gmx.at>
2024-04-26 21:03:40 +0200

    extract crawling headings into seperate method

--- WebCrawler/src/main/java/org/example/Crawler.java
+++ WebCrawler/src/main/java/org/example/Crawler.java
@@ -39,44 +39,8 @@ public class Crawler {
         }
 
         System.out.println(indent + "URL: " + url);
-        //Elements headings = document.select("h1, h2, h3, h4, h5, h6");
-        //for (Element heading : headings) {
-        //    System.out.println(indent + "  " + heading.tagName() + ": " + heading.text());
-        //}
-        Elements headings1 = document.select("h1");
-        /*
-        for (int i = 1; i < 7; i++) {
-            String head = "h" + i;
-            Elements h = document.select(head);
-            for (Element hd : h) {
-                System.out.println(indent + "  ##" + hd.tagName() + ": " + hd.text());
-            }
-        }*/
-        for (Element heading1 : headings1) {
-            System.out.println(indent + "  #" + heading1.tagName() + ": " + heading1.text());
-            Elements headings2 = document.select("h2");
-            for (Element heading2 : headings2) {
-                System.out.println(indent + "  ##" + heading2.tagName() + ": " + heading2.text());
-                Elements headings3 = document.select("h3");
-                for (Element heading3 : headings3) {
-                    System.out.println(indent + "  ###" + heading3.tagName() + ": " + heading3.text());
-                    Elements headings4 = document.select("h4");
-                    for (Element heading4 : headings4) {
-                        System.out.println(indent + "  ####" + heading4.tagName() + ": " + heading4.text());
-                        Elements headings5 = document.select("h5");
-                        for (Element heading5 : headings5) {
-                            System.out.println(indent + "  #####" + heading5.tagName() + ": " + heading5.text());
-                            Elements headings6 = document.select("h6");
-                            for (Element heading6 : headings6) {
-                                System.out.println(indent + "  ######" + heading6.tagName() + ": " + heading6.text());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        //crawlHeadings(headings, indent);
 
+        crawlHeadings(document, "h1", indent);
 
         Elements links = document.select("a[href]");
         //crawlLinks(links, indent);
@@ -88,12 +52,19 @@ public class Crawler {
         }
     }
 
-    private void crawlHeadings(Elements headings, String indent, Document document, String ints) {
-
-        for (Element head : headings) {
-            System.out.println(indent + "  ##" + head.tagName() + ": " + head.text());
+    public static void crawlHeadings(Document document, String tag, String indent) {
+        if (tag.equals("h7")) {
+            return;
         }
+        String newIndent = indent + "#";
+        Elements headings = document.select(tag);
+        for (Element heading : headings) {
+            System.out.println(newIndent + heading.tagName() + ": " + heading.text());
 
+            String nextTag = "h" + (Integer.parseInt(tag.substring(1)) + 1);
+
+            crawlHeadings(document, nextTag, newIndent);
+        }
     }
 
     private void crawlLinks(Elements links, String indent) {
